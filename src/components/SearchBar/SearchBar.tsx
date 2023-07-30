5c22f020b101712e766401e30df1f9172d1faf00
Tiphaine Durand <tiphaine.durand@metyis.com>
2023-07-30 03:43:20 +0200

    ✨ Enable SearchBar with Maps API and MUI

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,21 +1,161 @@
-import { Autocomplete, TextField } from '@mui/material'
+import { Autocomplete, Box, Grid, TextField, Typography, debounce } from '@mui/material'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import parse from 'autosuggest-highlight/parse';
 import './SearchBar.scss'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+
+const loadScript = (src: string, position: HTMLElement | null, id: string) => {
+  if (!position) {
+    return;
+  }
+
+  const script = document.createElement('script');
+  script.setAttribute('async', '');
+  script.setAttribute('id', id);
+  script.src = src;
+  position.appendChild(script);
+}
+
+const autocompleteService = { current: null };
+
+interface MainTextMatchedSubstrings {
+  offset: number;
+  length: number;
+}
+interface StructuredFormatting {
+  main_text: string;
+  secondary_text: string;
+  main_text_matched_substrings?: readonly MainTextMatchedSubstrings[];
+}
+interface PlaceType {
+  description: string;
+  structured_formatting: StructuredFormatting;
+}
+
+const MAPS_API_KEY = import.meta.env.VITE_MAPS_API_KEY
 
 const SearchBar = () => {
-  return <div className='searchBar'>
-    <Autocomplete
-      disablePortal
-      freeSolo
-      options={[]}
-      renderInput={(params) => (
-        <TextField {...params}
-        sx={{ backgroundColor: 'white' }}
-        label="Enter a city name"
-        margin="normal"
-      />)
+  const [value, setValue] = useState<PlaceType | null>(null);
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState<readonly PlaceType[]>([]);
+  const loaded = useRef(false);
+  
+  if (typeof window !== 'undefined' && !loaded.current) {
+    if (!document.querySelector('#google-maps')) {
+      loadScript(
+        `https://maps.googleapis.com/maps/api/js?key=${MAPS_API_KEY}&libraries=places&callback=Function.prototype`,
+        document.querySelector('head'),
+        'google-maps',
+      );
     }
+    loaded.current = true;
+  }
+
+  const fetch = useMemo(
+    () =>
+      debounce(
+        (
+          request: { input: string },
+          callback: (results?: readonly PlaceType[]) => void,
+        ) => {
+          (autocompleteService.current as any).getPlacePredictions(
+            request,
+            callback,
+          );
+        },
+        400,
+      ),
+    [],
+  );
+
+  useEffect(() => {
+    let active = true;
+
+    if (!autocompleteService.current && (window as any).google) {
+      autocompleteService.current = new (
+        window as any
+      ).google.maps.places.AutocompleteService();
+    }
+    if (!autocompleteService.current) return undefined;
+
+    if (inputValue === '') {
+      setOptions(value ? [value] : []);
+      return undefined;
+    }
+
+    fetch({ input: inputValue }, (results?: readonly PlaceType[]) => {
+      if (active) {
+        let newOptions: readonly PlaceType[] = [];
+        if (value) newOptions = [value];
+        if (results) newOptions = [...newOptions, ...results];
+        setOptions(newOptions);
+      }
+    });
+
+    return () => {
+      active = false;
+    };
+  }, [value, inputValue, fetch]);
+
+  return (
+    <Autocomplete
+      id="searchBar"
+      sx={{ width: '60%'}}
+      getOptionLabel={(option) =>
+        typeof option === 'string' ? option : option.description
+      }
+      filterOptions={(x) => x}
+      options={options}
+      autoComplete
+      includeInputInList
+      filterSelectedOptions
+      value={value}
+      noOptionsText="No locations"
+      onChange={(event: any, newValue: PlaceType | null) => {
+        setOptions(newValue ? [newValue, ...options] : options);
+        setValue(newValue);
+      }}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Add a location" fullWidth margin='normal' sx={{ backgroundColor: 'white', borderRadius: '5px' }}/>
+      )}
+      renderOption={(props, option) => {
+        const matches =
+          option.structured_formatting.main_text_matched_substrings || [];
+
+        const parts = parse(
+          option.structured_formatting.main_text,
+          matches.map((match: any) => [match.offset, match.offset + match.length]),
+        );
+
+        return (
+          <li {...props}>
+            <Grid container alignItems="center">
+              <Grid item sx={{ display: 'flex', width: 44 }}>
+                <LocationOnIcon sx={{ color: 'text.secondary' }} />
+              </Grid>
+              <Grid item sx={{ width: 'calc(100% - 44px)', wordWrap: 'break-word' }}>
+                {parts.map((part, index) => (
+                  <Box
+                    key={index}
+                    component="span"
+                    sx={{ fontWeight: part.highlight ? 'bold' : 'regular' }}
+                  >
+                    {part.text}
+                  </Box>
+                ))}
+                <Typography variant="body2" color="text.secondary">
+                  {option.structured_formatting.secondary_text}
+                </Typography>
+              </Grid>
+            </Grid>
+          </li>
+        );
+      }}
     />
-  </div>
+  );
 }
 
 export default SearchBar
