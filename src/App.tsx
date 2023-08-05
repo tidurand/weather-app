e77c6929d3164de4ebfed165eb7e62439f7fc265
Tiphaine Durand <tiphaine.durand@metyis.com>
2023-08-05 12:31:43 +0200

    🎨 Move useCity from WeatherDisplayer to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import SearchBar from './components/SearchBar/SearchBar'
 import WeatherDisplayer from './components/WeatherDisplayer/WeatherDisplayer'
 import './App.scss'
 import { useState } from 'react'
+import useCity from './hooks/useCity'
+import { fakeData } from './utils/fakeData'
 
 function App() {
   const [city, setCity] = useState('')
+  const cityData = useCity(city)
 
   return (
     <div className='App'>
       <SearchBar setCity={setCity} />
-      <WeatherDisplayer city={city} />
+      <WeatherDisplayer cityData={cityData || fakeData} />
     </div>
   )
 }
--- src/components/WeatherDisplayer/WeatherDisplayer.tsx
+++ src/components/WeatherDisplayer/WeatherDisplayer.tsx
@@ -1,25 +1,23 @@
 import './WeatherDisplayer.scss'
 import HourlyCard from './partials/HourlyCard'
 import MainCard from './partials/MainCard'
-import useCity from '../../hooks/useCity'
-import { fakeData } from '../../utils/fakeData'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper/modules'
 import 'swiper/scss'
 import 'swiper/scss/navigation'
+import { CityData } from '../../interfaces'
 
 interface WeatherDisplayerProps {
-  city: string
+  cityData: CityData
 }
 
-const WeatherDisplayer: React.FC<WeatherDisplayerProps> = ({ city }) => {
-  const cityData = useCity(city)
+const WeatherDisplayer: React.FC<WeatherDisplayerProps> = ({ cityData }) => {
   const getHours = new Date().getHours()
 
   return (
     <div className='weatherDisplayer'>
-      <p className='dayText'>{cityData?.location || fakeData.location}</p>
-      <MainCard data={cityData || fakeData} />
+      <p className='dayText'>{cityData.location}</p>
+      <MainCard data={cityData} />
       <Swiper
         modules={[Navigation]}
         navigation
@@ -33,12 +31,6 @@ const WeatherDisplayer: React.FC<WeatherDisplayerProps> = ({ city }) => {
             <HourlyCard key={index} index={index} data={hour} />
           </SwiperSlide>
         ))}
-        {!cityData &&
-          fakeData?.day[0].hourly.map((hour, index) => (
-            <SwiperSlide key={index}>
-              <HourlyCard key={index} index={index} data={hour} />
-            </SwiperSlide>
-          ))}
       </Swiper>
     </div>
   )
